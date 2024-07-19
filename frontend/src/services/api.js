c5c00266a422e840dc5e9e345da6fb9eb569a945
satan sharma <137980612+satan-furation@users.noreply.github.com>
2024-07-19 15:52:45 +0530

    Create api.js

---
+++ frontend/src/services/api.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+// const API_BASE_URL = "http://localhost:8080/api";
+const API_BASE_URL = "https://rent-vehicle-backend.vercel.app/api";
+
+export const getVehicleTypes = async (wheels) => {
+  const response = await axios.get(`${API_BASE_URL}/vehicle-types/${wheels}`);
+  return response.data; 
+};
+
+export const getVehicles = async (typeId) => {
+  const response = await axios.get(`${API_BASE_URL}/vehicles/${typeId}`);
+  return response.data;
+};
+
+export const submitBooking = async (data) => {
+  const response = await axios.post(`${API_BASE_URL}/bookings`, data);
+  return response.data;
+};
